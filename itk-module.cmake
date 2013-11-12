d50575d9b34991b2bd5793268d5b8ec0e0b42e41
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-11-12 11:13:33 -0500

    By default the modules will not be enabled.

--- itk-module.cmake
+++ itk-module.cmake
@@ -3,6 +3,13 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
+
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
 # itk_module() defines the module dependencies in ExternalTemplate
 # ExternalTemplate depends on ITKCommon
 # The testing module in ExternalTemplate depends on ITKTestKernel
@@ -19,5 +26,5 @@ itk_module(ExternalTemplate
     ITKMetaIO
   DESCRIPTION
     "${DOCUMENTATION}"
-  EXCLUDE_FROM_ALL
+  ${_EXCLUDE}
 )
