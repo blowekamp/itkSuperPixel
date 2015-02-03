19cc1f53a904f9bb041208a474ac4d6016e2820a
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-02-02 20:53:57 -0500

    Removing source directory

---
+++ include/itkSLICImageFilter.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkSLICImageFilter_h
+#define itkSLICImageFilter_h
+
+#include "itkImageToImageFilter.h"
+
+namespace itk
+{
+
+template< typename TInputImage, typename TOutputImage >
+class SLICImageFilter:
+    public ImageToImageFilter< TInputImage, TOutputImage >
+{
+public:
+  /** Standard class typedefs. */
+  typedef SLICImageFilter                                 Self;
+  typedef ImageToImageFilter< TInputImage, TOutputImage > Superclass;
+  typedef SmartPointer< Self >                            Pointer;
+  typedef SmartPointer< const Self >                      ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ExtractImageFilter, ImageToImageFilter);
+
+  /** Image type information. */
+  typedef TInputImage  InputImageType;
+  typedef TOutputImage OutputImageType;
+
+protected:
+  SLICImageFilter() {}
+  ~SLICImageFilter() {}
+
+  void PrintSelf(std::ostream & os, Indent indent) const ITK_OVERRIDE
+    {
+      Superclass::PrintSelf(os, indent);
+    }
+
+  void GenerateData() ITK_OVERRIDE
+    {
+    }
+
+private:
+  SLICImageFilter(const Self &);    //purposely not implemented
+  void operator=(const Self &);     //purposely not implemented
+
+};
+} // end namespace itk
+
+#endif //itkSLICImageFilter_h
